8cc80bd6c3c3fdb7b93541068bde6fdefa998e72
pollux <125819390+Pollux@users.noreply.github.com>
2025-05-12 14:29:59 +0400

    create file for work with api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,9 +4,10 @@ class APIUtil {
   final dio = Dio();
   final baseUrl = "https://rickandmortyapi.com/api";
 
-  Future<List> getCharacters() async {
-    final result = await dio.get("$baseUrl/character");
+  Future<List?> getCharacters([int page = 1]) async {
+    final result = await dio.get("$baseUrl/character/?page=$page");
     final List charactersList = result.data["results"] as List;
+    print(charactersList);
     return charactersList;
   }
 }
