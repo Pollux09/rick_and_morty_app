d07516ed7fbf8ca12a0739b47fd143b0f848113c
pollux <125819390+Pollux@users.noreply.github.com>
2025-05-12 14:28:54 +0400

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty_app/screens/favorite_screen.dart';
-import 'package:rick_and_morty_app/screens/main_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:rick_and_morty_app/hive/character.dart';
+import 'hive/hive_service.dart';
+import 'screens/favorite_screen.dart';
+import 'screens/main_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: "/",
-    routes: {
-      "/": (context) => const MainScreen(),
-      "/favorites": (context) => const FavoritesScreen(),
-    },
-  ));
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(CharacterAdapter());
+
+  await Hive.openBox<Character>("characters");
+  final hiveService = HiveService();
+  runApp(MyApp(hiveService: hiveService));
+}
+
+class MyApp extends StatelessWidget {
+  final HiveService hiveService;
+
+  const MyApp({super.key, required this.hiveService});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => MainScreen(),
+        "/favorites": (context) => FavoritesScreen(hiveService: hiveService),
+      },
+    );
+  }
 }
