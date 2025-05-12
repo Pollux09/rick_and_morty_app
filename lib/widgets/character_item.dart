56ceeea74c0e7b21e6b721d2f6112ffc9c1eb000
pollux <125819390+Pollux@users.noreply.github.com>
2025-05-12 19:39:49 +0400

    update character item

--- lib/widgets/character_item.dart
+++ lib/widgets/character_item.dart
@@ -1,5 +1,4 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rick_and_morty_app/characters/characters_bloc.dart';
@@ -7,6 +6,7 @@ import 'package:rick_and_morty_app/characters/characters_bloc.dart';
 class CharacterItem extends StatelessWidget {
   final String characterName;
   final Uint8List characterImage;
+  final String gender;
   final bool isFavorite;
   final dynamic characterKey;
 
@@ -14,51 +14,93 @@ class CharacterItem extends StatelessWidget {
     super.key,
     required this.characterName,
     required this.characterImage,
+    required this.gender,
     required this.isFavorite,
     required this.characterKey,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    IconData genderIcon;
+    Color genderColor;
+
+    if (gender == "Male") {
+      genderIcon = Icons.male;
+      genderColor = Colors.blueAccent;
+    } else if (gender == "Female") {
+      genderIcon = Icons.female;
+      genderColor = Colors.pinkAccent;
+    } else {
+      genderIcon = Icons.help_outline;
+      genderColor = Colors.white;
+    }
+
+    return Card(
+      color: const Color.fromARGB(255, 30, 30, 30),
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Stack(
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: Image.memory(characterImage, width: 96.0),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                characterName,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.memory(
+                    characterImage,
+                    width: 100.0,
+                    height: 100.0,
+                    fit: BoxFit.cover,
+                  ),
                 ),
+                const SizedBox(width: 16.0),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 56.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          characterName,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                        const SizedBox(height: 8.0),
+                        Icon(genderIcon, color: genderColor),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 8,
+            right: 8,
+            child: IconButton(
+              icon: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                color: Colors.amberAccent,
+                size: 36,
               ),
-              IconButton(
-                icon: isFavorite
-                    ? Icon(Icons.star, color: Colors.lightBlueAccent)
-                    : Icon(Icons.star_border, color: Colors.lightBlueAccent),
-                onPressed: () {
-                  if (!isFavorite) {
-                    context.read<CharactersBloc>().add(
-                      ToFavorite(characterKey),
-                    );
-                  } else {
-                    context.read<CharactersBloc>().add(
-                      DeleteFavoriteCharacter(characterKey: characterKey),
-                    );
-                  }
-                },
-                iconSize: 42.0,
-              ),
-            ],
+              onPressed: () {
+                final bloc = context.read<CharactersBloc>();
+                if (isFavorite) {
+                  bloc.add(
+                    DeleteFavoriteCharacter(characterName: characterName),
+                  );
+                } else {
+                  bloc.add(ToFavorite(characterName));
+                }
+              },
+            ),
           ),
         ],
       ),
